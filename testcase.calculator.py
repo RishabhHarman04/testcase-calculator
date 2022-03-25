da82e4e42aa7f2e015337e43ec2c6deb2a477054
RishabhHarman04 <er.23rishabh@gmail.com>
2022-03-25 14:58:32 +0530

    calculator

--- testcase.calculator.py
+++ testcase.calculator.py
@@ -11,5 +11,17 @@ class TestCalculator(unittest.TestCase):
         y=20
         result = calculator.sub(x,y)
         self.assertEqual(result, x-y)
+
+    def test_mult(self):
+        x = 10
+        y = 20
+        result = calculator.mult(x, y)
+        self.assertEqual(result, x*y)
+
+    def test_div(self):
+        x = 10
+        y = 20
+        result = calculator.div(x, y)
+        self.assertEqual(result, x/y)
 if __name__ == '__main__':
         unittest.main()
